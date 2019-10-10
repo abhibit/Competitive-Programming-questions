df6905b857e66106c882ab2c602b52b45dca2c5d
abhibit <32648154+abhibit@users.noreply.github.com>
2019-10-10 20:59:32 +0530

    knapsack with an example input and more documentation.

--- 0-1 Knapsack Problem/Solution.c
+++ 0-1 Knapsack Problem/Solution.c
@@ -1,37 +1,37 @@
-#include<stdio.h>
-#include<stdlib.h>
-
-int max(int a, int b)
-{
-	if (a>b)
-		return a;
-	else
-		return b;
-}
-
-int main()
-{
-	int n, value[100], capacity, weight[100],i,j,K[100][100];
-	printf("Input number of  items:");
-	scanf("%d",&n);
-	printf("Enter values:");
-	for(i=0;i<n;i++)
-		scanf("%d",&value[i]);
-	printf("Enter weights:");
-	for(i=0;i<n;i++)
-		scanf("%d",&weight[i]);	
-	printf("Enter Capacity:");
-	scanf("%d",&capacity);
-	for(i=0;i<=n;i++)
-		for(j=0;j<=capacity;j++)
-		{
-			if(i==0 || j==0)
-				K[i][j]=0;
-			else if (weight[i-1]<=j)
-				K[i][j]=max(K[i-1][j],K[i-1][j-weight[i-1]]+value[i-1]);
-			else
-				K[i][j]=K[i-1][j];
-		}	
-	printf("%d",K[n][capacity]);
-	return 0;
-}
+#include<stdio.h> 
+  
+// A utility function that returns maximum of two integers 
+int max(int a, int b) { return (a > b)? a : b; } 
+  
+// Returns the maximum value that can be put in a knapsack of capacity W 
+int knapSack(int W, int wt[], int val[], int n) 
+{ 
+   int i, w; 
+   int K[n+1][W+1]; 
+  
+   // Build table K[][] in bottom up manner 
+   for (i = 0; i <= n; i++) 
+   { 
+       for (w = 0; w <= W; w++) 
+       { 
+           if (i==0 || w==0) 
+               K[i][w] = 0; 
+           else if (wt[i-1] <= w) 
+                 K[i][w] = max(val[i-1] + K[i-1][w-wt[i-1]],  K[i-1][w]); 
+           else
+                 K[i][w] = K[i-1][w]; 
+       } 
+   } 
+  
+   return K[n][W]; 
+} 
+  
+int main() 
+{ 
+    int val[] = {60, 100, 120}; 
+    int wt[] = {10, 20, 30}; 
+    int  W = 50; 
+    int n = sizeof(val)/sizeof(val[0]); 
+    printf("%d", knapSack(W, wt, val, n)); 
+    return 0; 
+} 
